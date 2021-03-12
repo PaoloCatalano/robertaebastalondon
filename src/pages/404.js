b5328c160c400a6a2f4986781fec8217e4816ee8
PaoloCatalano <paolo12587@gmail.com>
2021-03-12 20:54:25 +0000

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,33 @@
 import React from "react"
-import { Redirect } from "@reach/router"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
+import Sidebar from "../components/Sidebar"
+import NavbarFixed from "../components/NavbarFixed"
 
-const NotFoundPage = () => <Redirect to="/" />
+const Error = () => {
+  return (
+    <div className="contact-page-body">
+      <NavbarFixed />
+      <Sidebar contacts></Sidebar>
+      <Layout oneModal>
+        <div className="contact-page">
+          <div className="phantom-navbar"></div>
+          <div className="phantom-column-collection"></div>
+          <div className="center">
+            <div className="contact-cascading">
+              <h3>404 ERROR</h3>
+              <div className="underline" />
+              <div className="space" />
+              <h1>Page not found.</h1>
+              <h2 style={{ margin: 50 }}>
+                <Link to="/">Back to the Home Page</Link>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
 
-export default NotFoundPage
+export default Error
